83a8eeed663f4a0ad667e5db27cfb3666f1d7398
Masami Ogoshi <ogochan@nurs.or.jp>
2019-03-30 14:34:05 +0000

    file view method

--- routes/notebooks.js
+++ routes/notebooks.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const is_authenticated = require('../modules/user.js').is_authenticated;
-
+const Content = require('../modules/content');
+const {User, is_authenticated} = require('../modules/user');
 const notebookFunc = function(req, res, next) {
-	//console.log(req.params.name);
-	res.render('notebooks', { title: 'Notebooks',
-							  name: path.basename(req.params.name),
-							  path: req.params.name,
-							  version_hash: '0.0'
-							});
+	let name = req.params.name;
+	console.log(name);
+	if ( name.match(/\.ipynb$/) ) {
+		res.render('notebooks', { title: 'Notebooks',
+								  name: path.basename(req.params.name),
+								  path: req.params.name,
+								  version_hash: '0.0'
+								});
+	} else {
+		let content = new Content(User.current(req), name);
+		content.load(false);
+	
+		//console.log(content.mime_type);
+		res.set('Content-Type', content.mime_type);
+		res.send(content.content);
+	}
 };
 
 /* GET users listing. */
