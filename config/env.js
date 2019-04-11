298537117acc1c6ea2b03cbc6d2d2cf65f778442
Masami Ogoshi <ogochan@nurs.or.jp>
2019-04-11 11:32:29 +0000

    use user table

---
+++ config/env.js
@@ -0,0 +1,15 @@
+module.exports =  {
+	connection_dir: `${process.env.XDG_RUNTIME_DIR}/jupyter`,
+	kernels_dir: `${process.env.HOME}/.ipython/kernels`,
+	home: process.env.HOME,
+	data_dir: `${process.env.HOME}/jupyter`,
+	host: '127.0.0.1',
+	shell_port: 0,
+	stdin_port: 0,
+	control_port: 0,
+	iopub_port: 0,
+	heartbeat_port: 0,
+	session_ttl: 3600 * 24 * 7,
+	session_path: './sessions',
+	password_path: './password',
+};
